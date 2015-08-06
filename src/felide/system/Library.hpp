2ea53526594ea65e87da49d83105bf744d755af6
Felipe Apablaza <fapablaza@adexus.cl>
2015-08-06 16:20:28 -0300

    [+] Added 'system' namespace.

---
+++ src/felide/system/Library.hpp
@@ -0,0 +1,25 @@
+/*
+ * Felipe's Integrated Development Environment.
+ *
+ * Copyright (c) 2014 Felipe Apablaza.
+ *
+ * This file and its source code is licensed under the GPLv3 license.
+ * For details, see the LICENSE file included in this software distribution.
+ */
+
+#ifndef __FELIDE_SYSTEM_LIBRARY_HPP__
+#define __FELIDE_SYSTEM_LIBRARY_HPP__
+
+#include <felide/system/Module.hpp>
+
+namespace felide { namespace system {
+	/**
+	 * @brief Dynamically loaded executable module library (.so on Unix, .dll on Windows)
+	 */
+	class Library : public Module {
+	public:
+		virtual ~Library();
+	};
+}}
+
+#endif
