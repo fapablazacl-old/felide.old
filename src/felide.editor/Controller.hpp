4bd501babfcff08c41336cf4da64985a06f56d65
Felipe Apablaza <fapablaza@adexus.cl>
2015-08-15 22:09:04 -0300

    [+] Added 'View' template class.

---
+++ src/felide.editor/Controller.hpp
@@ -0,0 +1,15 @@
+
+#ifndef __felide_editor_controller_hpp__
+#define __felide_editor_controller_hpp__
+
+namespace felide { namespace editor {
+	/*
+	template<typename View>
+	class Controller {
+	public:
+		virtual ~Controller() {}
+	};
+	*/
+}}
+
+#endif // __felide_editor_controller_hpp__
