7fdb9b7a244e65152bc71bb92785e90395ba31d0
Felipe Apablaza <fapablaza@adexus.cl>
2015-09-04 18:15:54 -0300

    [+] Added QDialogFactory class.

---
+++ src/felide.editor/qt5/QDialogFactory.hpp
@@ -0,0 +1,16 @@
+
+#ifndef __felide_editor_qt5_qdialogfactory_hpp__
+#define __felide_editor_qt5_qdialogfactory_hpp__
+
+#include "felide.editor/DialogFactory.hpp"
+
+namespace felide { namespace editor { namespace qt5 {
+
+    class QDialogFactory : public DialogFactory {
+    public:
+
+
+    };
+}}}
+
+#endif
